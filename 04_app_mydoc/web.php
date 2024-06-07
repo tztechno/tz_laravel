e74ba225a0a800aa7aa6a817716e637c57c549e6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-06-07 11:55:09 +0900

    Delete 04_app_mydoc directory

--- 04_app_mydoc/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DocumentController;
-
-Route::get('/', [DocumentController::class, 'index']);
-Route::get('/documents/{document}', [DocumentController::class, 'show'])->name('document.show');
