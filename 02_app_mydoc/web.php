fb177d56bcc9d5483ef1a9e07a5eeb96118b93ba
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-06-07 11:54:33 +0900

    Delete 02_app_mydoc directory

--- 02_app_mydoc/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DocumentController;
-
-Route::get('/', [DocumentController::class, 'index']);
-Route::get('/documents/{filename}', [DocumentController::class, 'show'])->where('filename', '.*');
